41538e8b40ad65f8838dc5d6898c4a7051ad7da1
Theppitak Karoonboonyanan <thep@linux.thai.net>
2013-08-17 15:38:52 +0700

    Allow Nikhahit + Tone sequence for Tai Noi
    * src/lx-tn-im-table.c (normal_action):
      - Change (AD1,T) action from R to A.

--- src/lx-tn-im-table.c
+++ src/lx-tn-im-table.c
@@ -98,7 +98,7 @@ static LxTNImAction normal_action[NC_TOTAL][NC_TOTAL] =
   {A, A, A, S, S, A, R, R, A, A, R, R, R, R, R, R}, /*BV2*/
   {A, A, A, S, S, A, R, R, R, R, R, R, R, R, R, R}, /*BD*/
   {A, A, A, A, A, A, R, R, A, R, R, R, R, R, R, R}, /*T*/
-  {A, A, A, S, S, A, R, R, R, R, R, R, R, R, R, R}, /*AD1*/
+  {A, A, A, S, S, A, R, R, R, A, R, R, R, R, R, R}, /*AD1*/
   {A, A, A, S, S, A, R, R, R, R, R, R, R, R, R, R}, /*AD2*/
   {A, A, A, S, S, A, R, R, R, R, R, R, R, R, R, R}, /*AD3*/
   {A, A, A, S, S, A, R, R, A, A, A, R, R, R, R, R}, /*AV1*/
